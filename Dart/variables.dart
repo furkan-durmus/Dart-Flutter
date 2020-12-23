34643518b8f419f8a03c9a87a67eed3c05e1bb02
Furkan Durmus <16furkandurmus@gmail.com>
2020-12-23 15:18:48 +0300

    dart

--- Dart/variables.dart
+++ Dart/variables.dart
@@ -7,4 +7,8 @@ void main() {
   print(book);
   print(grade);
   print(name);
+
+  final String example = "hi";
+  const String example2 = "hello";
+  // example = "hello"; We cannot change example or examle2 variable, couse they have been defined as "final" and "const"
 }
---
+++ methods.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num = 22;
+  double num2 = 4.3;
+  String text = "Hello world";
+//some methods ;
+  print(num.isEven);
+  print(num2.floor());
+  print(num2.ceil());
+  print(text.toUpperCase());
+  print(text.length);
+}
